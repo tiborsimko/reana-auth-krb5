c978c83a92c998660ffda544dd6bb5a1d2de535e
Tibor Šimko <tibor.simko@cern.ch>
2026-03-12 12:12:57 +0100

    ci(yamllint): add YAML linting checks (#22)

--- run-tests.sh
+++ run-tests.sh
@@ -63,12 +63,17 @@ lint_shellcheck() {
     find . -name "*.sh" -exec shellcheck {} \+
 }
 
+lint_yamllint() {
+    yamllint .
+}
+
 all() {
     docker_build
     docker_test
     lint_commitlint
     lint_hadolint
     lint_shellcheck
+    lint_yamllint
 }
 
 help() {
@@ -81,6 +86,7 @@ help() {
     echo "  --lint-commitlint    Check linting of commit messages"
     echo "  --lint-hadolint      Check linting of Dockerfiles"
     echo "  --lint-shellcheck    Check linting of shell scripts"
+    echo "  --lint-yamllint      Check linting of YAML files"
 }
 
 if [ $# -eq 0 ]; then
@@ -97,5 +103,6 @@ case $arg in
 --lint-commitlint) lint_commitlint "$@" ;;
 --lint-hadolint) lint_hadolint ;;
 --lint-shellcheck) lint_shellcheck ;;
+--lint-yamllint) lint_yamllint ;;
 *) echo "[ERROR] Invalid argument '$arg'. Exiting." && help && exit 1 ;;
 esac
